ce3836a0045d285398e1fe95023b15841e031844
Skye J <skyeneet@protonmail.com>
2020-10-04 10:41:31 -0400

    Updated config, help message

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ message = "**RECIEVER**, you got a VERB from **SENDER**"
 
 selfMessage = "**SENDER**, please don't do that to yourself..."
 selfImage = "https://wallpapercave.com/wp/wp2098611.jpg"
-selfHarmVerbs = ["kill", "slash", "punch", "stab", "shoot"]
+selfHarmVerbs = ["kill", "slash", "punch", "stab", "shoot", "murder", "smack", "bonk"]
 
 noGifFound = "https://wallpapercave.com/wp/wp2098611.jpg"
 
 
-hearts = ["heart", "purple_heart", "white_heart", "green_heart", "yellow_heart", "black_heart", "brown_heart", "blue_heart", "orange_heart"]
+hearts = ["heart", "purple_heart", "white_heart", "green_heart", "yellow_heart", "black_heart", "brown_heart", "blue_heart", "orange_heart", "heartpulse", "sparkling_heart", "gift_heart", "cupid"]
 
 helpMessage = "Mention Commands:\n\
 ```\n\
@@ -24,6 +24,8 @@ helpMessage = "Mention Commands:\n\
     - List all tags within a verb\n\
 @Shleep Bot choose <choice> | <choice> | <choice>\n\
     - Choose from any number of choices!\n\
+@Shleep Bot eightball <question>\n\
+    - Let Shleep Bot answer all your toughest inquiries\n\
 ```\n\
 Admin Commands:\n\
 ```\n\
@@ -33,6 +35,8 @@ Admin Commands:\n\
     - Deletes a verb and all images contained within!\n\
 @Shleep Bot delete gif <verb> <url>\n\
     - Deletes a gif with the given verb + url\n\
+@Shleep Bot synonym: <verb1> <verb2>\n\
+    - Creates a \"synonym\" of verb1, verb2\n\
 @Shleep Bot dump\n\
     - Posts a yaml file of the current database\n\
 @Shleep Bot prune\n\
